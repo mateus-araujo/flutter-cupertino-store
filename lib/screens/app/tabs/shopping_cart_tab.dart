69c9a0f3e46437c84fe8f0cd4842f53c03126b8f
Mateus Araujo <mateus_1542@hotmail.com>
2021-03-20 12:34:08 -0700

    feat(shopping-cart): add date and time picker

--- lib/screens/app/tabs/shopping_cart_tab.dart
+++ lib/screens/app/tabs/shopping_cart_tab.dart
@@ -1,8 +1,12 @@
 import 'package:cupertino_store/components/cupertino_text_input.dart';
 import 'package:cupertino_store/models/app/app_state_model.dart';
+import 'package:cupertino_store/theme/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+
+const double _kDateTimePickerHeight = 216;
 
 class ShoppingCartTab extends StatefulWidget {
   @override
@@ -72,6 +76,52 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     );
   }
 
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 8, 19, 24),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const <Widget>[
+                  Icon(
+                    CupertinoIcons.clock,
+                    color: CupertinoColors.lightBackgroundGray,
+                    size: 28,
+                  ),
+                  SizedBox(width: 6),
+                  Text(
+                    'Delivery time',
+                    style: Styles.deliveryTimeLabel,
+                  ),
+                ],
+              ),
+              Text(
+                DateFormat.yMMMd().add_jm().format(dateTime!),
+                style: Styles.deliveryTime,
+              ),
+            ],
+          ),
+          Container(
+            height: _kDateTimePickerHeight,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.dateAndTime,
+              initialDateTime: dateTime,
+              onDateTimeChanged: (newDateTime) {
+                setState(() {
+                  dateTime = newDateTime;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
@@ -89,6 +139,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
                   _buildNameField(),
                   _buildEmailField(),
                   _buildLocationField(),
+                  _buildDateAndTimePicker(context),
                 ]),
               ),
             ),
