f4ec65b52e0752049b7e3dbf811931547f93debc
Mateus Araujo <mateus_1542@hotmail.com>
2021-03-20 14:52:54 -0700

    feat(shopping-cart): implements products in cart list

--- lib/screens/app/tabs/shopping_cart_tab.dart
+++ lib/screens/app/tabs/shopping_cart_tab.dart
@@ -1,4 +1,5 @@
 import 'package:cupertino_store/components/cupertino_text_input.dart';
+import 'package:cupertino_store/components/shopping_cart_item.dart';
 import 'package:cupertino_store/models/app/app_state_model.dart';
 import 'package:cupertino_store/theme/styles.dart';
 import 'package:flutter/cupertino.dart';
@@ -21,6 +22,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   String? location;
   String? pin;
   DateTime? dateTime = DateTime.now();
+  final _currencyFormat = NumberFormat.currency(symbol: '\$');
 
   Widget _buildInputPadding(Widget input) {
     return Padding(
@@ -122,6 +124,75 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     );
   }
 
+  // Widget _buildProductInCart(AppStateModel model, int productIndex) {}
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate(
+      (context, index) {
+        final productIndex = index - 4;
+
+        switch (index) {
+          case 0:
+            return _buildNameField();
+          case 1:
+            return _buildEmailField();
+          case 2:
+            return _buildLocationField();
+          case 3:
+            return _buildDateAndTimePicker(context);
+          default:
+            if (model.productsInCart.length > productIndex) {
+              final productsIds = model.productsInCart.keys.toList();
+              final productsQuantities = model.productsInCart.values.toList();
+
+              return ShoppingCartItem(
+                index: productIndex,
+                product: model.getProductById(productsIds[productIndex]),
+                quantity: productsQuantities[productIndex],
+                lastItem: productIndex == model.productsInCart.length - 1,
+                formatter: _currencyFormat,
+              );
+            } else if (model.productsInCart.keys.length == productIndex &&
+                model.productsInCart.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 60),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Shipping '
+                            '${_currencyFormat.format(model.shippingCost)}',
+                            style: Styles.productRowItemPrice,
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            'Tax ${_currencyFormat.format(model.tax)}',
+                            style: Styles.productRowItemPrice,
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            'Total ${_currencyFormat.format(model.totalCost)}',
+                            style: Styles.productRowTotal,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+        }
+        return null;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
@@ -135,12 +206,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               top: false,
               minimum: const EdgeInsets.only(top: 4),
               sliver: SliverList(
-                delegate: SliverChildListDelegate(<Widget>[
-                  _buildNameField(),
-                  _buildEmailField(),
-                  _buildLocationField(),
-                  _buildDateAndTimePicker(context),
-                ]),
+                delegate: _buildSliverChildBuilderDelegate(model),
               ),
             ),
           ],
